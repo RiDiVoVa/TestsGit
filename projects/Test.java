fb51721bd28e190764c27378b8fffc86c20c394f
RiDiVoVa <ridivova8@gmail.com>
2022-11-21 18:30:38 +0200

    change Test

--- projects/Test.java
+++ projects/Test.java
@@ -2,6 +2,7 @@ package projects;
 
 public class Test{
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello Jina and Ali");
+        System.out.println("my friends");
     }
 }
