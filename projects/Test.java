1f2e38872f4019371e7233584e36c53cbbe5a7d3
RiDiVoVa <ridivova8@gmail.com>
2022-11-21 23:06:56 +0200

    fefm

--- projects/Test.java
+++ projects/Test.java
@@ -4,6 +4,6 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("hello vovvvvvvvvvvvvvvvv");
        int i =9;
-        int o = 10;
+       int b =9;
     }
 }
