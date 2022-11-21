a6cd2adf674dfb65b276c00a7b58de83fb4d8a85
RiDiVoVa <ridivova8@gmail.com>
2022-11-21 18:56:34 +0200

    bug fix 2

--- projects/Test.java
+++ projects/Test.java
@@ -2,6 +2,14 @@ package projects;
 
 public class Test{
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
+        System.out.println("hello vovvvvvvvvvvvvvvvv");
     }
 }
