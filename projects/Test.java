0753b2d34714e1290c18b355ac007c440786a3da
RiDiVoVa <117299814+RiDiVoVa@users.noreply.github.com>
2022-11-21 22:48:11 +0200

    Update Test.java

--- projects/Test.java
+++ projects/Test.java
@@ -3,12 +3,6 @@ package projects;
 public class Test{
     public static void main(String[] args) {
         System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
-        System.out.println("hello vovvvvvvvvvvvvvvvv");
+       
     }
 }
