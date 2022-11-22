f4e613a061a8c043a35e31143f8cc151911fc13c
RiDiVoVa <ridivova8@gmail.com>
2022-11-22 22:13:46 +0200

    add

--- projects/Test.java
+++ projects/Test.java
@@ -4,6 +4,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("hello vovvvvvvvvvvvvvvvv");
        int i =9;
-       int b =9;
+       int b =10;
+       int j = 11;
     }
 }
